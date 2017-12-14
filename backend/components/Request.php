f09b245b77153b153ed412bf573b25c432ab19a8
Renick <renick.buettner@me.com>
2017-12-14 21:44:12 +0100

    Starting implementing Requests;

---
+++ backend/components/Request.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renickbuettner
+ * Date: 14.12.17
+ * Time: 21:41
+ */
+
+namespace renickbuettner\App;
+
+
+class Request
+{
+    private $methode;
+
+    public function __construct(RequestMethod $methode)
+    {
+    }
+
+}
